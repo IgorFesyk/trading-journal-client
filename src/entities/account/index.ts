18d42aa19d425d0e3b931a8b00b1034ba88fbacd
Ihor Fesyk <igor.fesyk.g@gmail.com>
2026-07-15 22:20:29 +0300

    switched to npm

--- src/entities/account/index.ts
+++ src/entities/account/index.ts
@@ -1,3 +1,5 @@
 export type { Account, AccountType, Currency } from './account.type'
 export { createAccountApi } from './api/create-account.api'
 export { accountQueryKeys, accountQueries } from './account.queries'
+
+export { getAccountStatsApi } from './api/get-account-stats.api'
